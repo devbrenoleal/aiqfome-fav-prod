1fcb5d316fa7881961521aff82b97c13d6213c1f
Breno <brenoluizleal@gmail.com>
2025-08-23 16:39:14 -0300

    Acrescenta construtor utilitário nas classes exception

--- src/main/java/com/aiqfome/demo/exception/BusinessException.java
+++ src/main/java/com/aiqfome/demo/exception/BusinessException.java
@@ -1,3 +1,7 @@
 package com.aiqfome.demo.exception;
 
-public class BusinessException extends RuntimeException { }
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/aiqfome/demo/exception/UnexpectedException.java
+++ src/main/java/com/aiqfome/demo/exception/UnexpectedException.java
@@ -1,3 +1,8 @@
 package com.aiqfome.demo.exception;
 
-public class UnexpectedException extends RuntimeException { }
+public class UnexpectedException extends RuntimeException {
+
+    public UnexpectedException(String message) {
+        super(message);
+    }
+}
