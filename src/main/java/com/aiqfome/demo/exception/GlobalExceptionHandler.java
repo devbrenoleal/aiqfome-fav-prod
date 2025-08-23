9b87cf45488c045d2719ba7923f8b6f3c7237045
Breno <brenoluizleal@gmail.com>
2025-08-23 15:36:21 -0300

    Implementação do `GlobalExceptionHandler` para retornar validação de campos

---
+++ src/main/java/com/aiqfome/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.aiqfome.demo.exception;
+
+import com.aiqfome.demo.dto.ErrorDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .toList();
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
