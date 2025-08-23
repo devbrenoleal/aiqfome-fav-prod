cd550f21c9f2dcfe4888910a8b7437157e48b4cf
Breno <brenoluizleal@gmail.com>
2025-08-23 14:50:34 -0300

    Cria as classes de exceção `BusinessException` e `UnexpectedException`

---
+++ src/main/java/com/aiqfome/demo/exception/BusinessException.java
@@ -0,0 +1,3 @@
+package com.aiqfome.demo.exception;
+
+public class BusinessException extends RuntimeException { }
---
+++ src/main/java/com/aiqfome/demo/exception/UnexpectedException.java
@@ -0,0 +1,3 @@
+package com.aiqfome.demo.exception;
+
+public class UnexpectedException extends RuntimeException { }
